eb43cd847faa7b0c2165b414f65d1c13fc5782eb
Jason Ko <jekb2019@gmail.com>
2021-10-29 20:55:26 +1300

    Add: Create BasicButton component and stories

---
+++ src/components/BasicButton/BasicButton.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './BasicButton.module.css';
+
+const BasicButton = ({ size, label, onClick, backgroundColor, color }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`
+          ${styles.btn} 
+          ${styles[`background_${backgroundColor}`]}
+          ${styles[color]}
+          ${styles[size]}
+        `}
+    >
+      {label}
+    </button>
+  );
+};
+
+BasicButton.propTyles = {
+  size: PropTypes.oneOf(['small']),
+  label: PropTypes.string,
+  onClick: PropTypes.func,
+  backgroundColor: PropTypes.oneOf(['red, green']),
+  color: PropTypes.oneOf(['white']),
+};
+
+export default BasicButton;
---
+++ src/components/BasicButton/BasicButton.stories.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import BasicButton from './BasicButton';
+
+export default {
+  title: 'Shared/BasicButton',
+  component: BasicButton,
+  args: {
+    color: 'white',
+    size: 'small',
+  },
+};
+
+const Template = (args) => <BasicButton {...args} />;
+
+export const Primary_OK = Template.bind({});
+Primary_OK.args = {
+  label: 'OK',
+  backgroundColor: 'green',
+};
+
+export const Primary_Cancel = Template.bind({});
+Primary_Cancel.args = {
+  label: 'Cancel',
+  backgroundColor: 'red',
+};
