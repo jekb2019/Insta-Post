822121cb84182c2d771fcd86d3c2b291c841c29c
Jason Ko <jekb2019@gmail.com>
2021-10-30 15:15:08 +1300

    Add: Create ProfileImg component and stories

---
+++ src/components/elements/ProfileImg/ProfileImg.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './ProfileImg.module.css';
+import testImg from '../../../assets/images/mockProfile.jpg';
+import PropTypes from 'prop-types';
+
+const ProfileImg = ({ size, borderColor }) => {
+  return (
+    <div
+      className={`${styles.container} ${styles[size]} ${
+        styles[`border_${borderColor}`]
+      }`}
+    >
+      <img className={`${styles.img}`} src={testImg} />
+    </div>
+  );
+};
+
+ProfileImg.propTypes = {
+  size: PropTypes.oneOf(['small, medium, large']),
+  borderColor: PropTypes.oneOf(['gray, red']),
+};
+
+export default ProfileImg;
