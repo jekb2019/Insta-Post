a2bd12cd32da300cf6b39c6557ac356dcafabce4
Jason Ko <jekb2019@gmail.com>
2021-10-31 14:54:25 +1300

    Add: Add styles to WithHeader HOC

--- src/pages/hoc/WithHeader/WithHeader.jsx
+++ src/pages/hoc/WithHeader/WithHeader.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import Header from '../../components/modules/Header/Header';
+import Header from '../../../components/modules/Header/Header';
+import styles from './WithHeader.module.css';
 
 /**
  * WithHeader is an HOC where it injects Header into a Page
  */
 const WithHeader = (Page) => {
   return () => (
-    <>
+    <div className={styles.container}>
       <Header name="Jason Ko" />
       <Page />
-    </>
+    </div>
   );
 };
 
