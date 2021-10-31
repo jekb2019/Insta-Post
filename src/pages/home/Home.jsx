e02475d865a4d0611d04b9de92ded190aff29db0
Jason Ko <jekb2019@gmail.com>
2021-10-31 15:25:13 +1300

    Add: Layout for Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import AddNewButton from '../../components/elements/AddNewButton/AddNewButton';
+import FriendList from '../../components/modules/FriendList/FriendList';
 import Post from '../../components/modules/Post/Post';
+import styles from './Home.module.css';
 
 const Home = (props) => (
-  <div>
-    <h1>Home Page</h1>
-    <Post />
+  <div className={styles.container}>
+    <main className={styles.posts_wrapper}>
+      <Post />
+    </main>
+    <aside className={styles.friend_list_container}>
+      <FriendList />
+    </aside>
+    <div className={styles.add_new_btn_container}>
+      <AddNewButton color="orange" size="large" />
+    </div>
   </div>
 );
 
