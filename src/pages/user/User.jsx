9c866e2fcd15d7b87049b0bfc41b961e239f4c34
Jason Ko <jekb2019@gmail.com>
2021-11-01 11:12:45 +1300

    Add: Create User page

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import MyProfile from '../../components/modules/MyProfile/MyProfile';
+import PostList from '../../components/modules/PostList/PostList';
+import posts from '../../mock/posts';
+import currentUser from '../../mock/user';
+import styles from './User.module.css';
+
+const myPosts = posts.filter(
+  (post) => post.authorUsername === currentUser.username
+);
 
 const User = (props) => (
-  <div>
-    <h1>User Page</h1>
+  <div className={styles.container}>
+    <div className={styles.content_wrapper}>
+      <div className={styles.profile}>
+        <MyProfile />
+      </div>
+      <div className={styles.posts}>
+        <PostList posts={myPosts} />
+      </div>
+    </div>
   </div>
 );
 
