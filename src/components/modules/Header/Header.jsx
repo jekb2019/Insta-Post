ad1dce47214bcac42bae6e7623d1d2864ac771e2
Jason Ko <jekb2019@gmail.com>
2021-10-31 15:02:59 +1300

    Add: Add sticky prop to Header

--- src/components/modules/Header/Header.jsx
+++ src/components/modules/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Header.module.css';
 import logo from '../../../assets/images/header_logo.png';
 import SearchBar from '../../elements/SearchBar/SearchBar';
 import ProfileImg from '../../elements/ProfileImg/ProfileImg';
 
-const Header = ({ name }) => (
-  <header className={styles.container}>
+const Header = ({ name, isSticky }) => (
+  <header className={`${styles.container} ${isSticky && styles.sticky}`}>
     <div className={styles.inner_container}>
       <img className={styles.logo} src={logo} alt="InstaPost logo" />
       <SearchBar />
@@ -16,5 +17,10 @@ const Header = ({ name }) => (
     </div>
   </header>
 );
+
+Header.propTypes = {
+  name: PropTypes.string,
+  isSticky: PropTypes.bool,
+};
 
 export default Header;
