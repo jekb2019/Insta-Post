225089af1c7a76ec3f0cff7198a96ec022971fb2
Jason Ko <jekb2019@gmail.com>
2021-10-31 17:32:04 +1300

    Update: Update Header and BasicButton style

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import AddNewButton from '../../components/elements/AddNewButton/AddNewButton';
 import FriendList from '../../components/modules/FriendList/FriendList';
-import Post from '../../components/modules/Post/Post';
 import PostList from '../../components/modules/PostList/PostList';
 import posts from '../../mock/posts';
 import styles from './Home.module.css';
