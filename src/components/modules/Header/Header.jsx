f106fbfb339871554bca205a06a88adf673647d6
Jason Ko <jekb2019@gmail.com>
2021-11-01 15:29:14 +1300

    Update: Add ProfileNav to Header

--- src/components/modules/Header/Header.jsx
+++ src/components/modules/Header/Header.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Header.module.css';
 import logo from '../../../assets/images/header_logo.png';
 import SearchBar from '../../elements/SearchBar/SearchBar';
 import ProfileImg from '../../elements/ProfileImg/ProfileImg';
+import ProfileNav from '../ProfileNav/ProfileNav';
+
+const navItemConfigs = [
+  {
+    label: 'My Page',
+    onClick: () => console.log('My Page clicked'),
+  },
+  {
+    label: 'Log out',
+    onClick: () => console.log('Log out clicked'),
+  },
+];
 
 const Header = ({ name, profileImg, isSticky }) => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <header className={`${styles.container} ${isSticky && styles.sticky}`}>
       <div className={styles.inner_container}>
         <img className={styles.logo} src={logo} alt="InstaPost logo" />
         <SearchBar />
-        <div className={styles.user}>
+        <div className={styles.user} onClick={toggleNav}>
           <p className={styles.name}>{name}</p>
           <ProfileImg borderColor="gray" size="medium" image={profileImg} />
         </div>
       </div>
+      {isNavOpen && (
+        <div className={styles.nav_container}>
+          <ProfileNav itemConfigs={navItemConfigs} />
+        </div>
+      )}
     </header>
   );
 };
