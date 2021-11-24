d36e3609e56e24960feb5cd5d79ac84107401810
Jason Ko <jekb2019@gmail.com>
2021-11-24 22:43:19 +1300

    Update: update API key

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "instapostdce163bc": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "instapost": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "instapostimgstorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
